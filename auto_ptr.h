e871960b8224912c79b48df72a63778f328c2868
DenisTrukhin <denistrukhin@mail.ru>
2017-04-06 19:44:58 +0300

    Description of auto_ptr class

---
+++ auto_ptr.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+#include "my_utils.h"
+//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+//! @file auto_ptr.h
+//! Implements an auto_ptr class
+//!
+//! @author Denis & Ilya, 2017
+//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+//! Auto_ptr class
+//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+template <typename T>
+class Auto_ptr
+{
+public:
+	Auto_ptr(const Auto_ptr& that)
+		:pointer_(that.pointer_)
+	{
+		that.pointer_ = nullptr;
+	};
+	~Auto_ptr()
+	{
+		PRINTOUT();
+		delete pointer_;
+		pointer_ = nullptr;
+	};
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Override of operator =
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	const Auto_ptr& operator=(const Auto_ptr& that)
+	{
+		pointer_ = that.pointer_;
+		that.pointer_ = nullptr;
+		return *this;
+	}
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Override of operator ->
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	T* operator ->()
+	{
+		return this->pointer_;
+	}
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Override of operator *
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	T& operator*()
+	{
+		return *pointer_;
+	}
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	//! Override of operator T*
+	//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+	operator T*()
+	{
+		T * pointer = pointer_;
+		pointer_ = nullptr;
+		return pointer;
+	}
+private:
+	T * pointer_;
+};
+
